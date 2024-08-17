811eda84d8f57332b8d783f3bd5842ff1940b20f
Rexfordd2 <64678115+Rexfordd2@users.noreply.github.com>
2024-08-17 05:05:08 +0500

    Sunrun-Website

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+document.getElementById('pdf_file').addEventListener('change', function() {
+  var fileName = this.files[0].name;
+  document.querySelector('.custom-file-label').textContent = fileName;
+});
